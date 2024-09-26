bc1150b410371e0f83844fc049346ddce71f9ed1
Aleksandr <sasha.syrota15@gmail.com>
2024-09-26 09:44:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-
 class Car:
     def __init__(self,
                  comfort_class: int,
@@ -22,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> None:
+    def serve_cars(self, cars: list[Car]) -> None:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -30,12 +27,12 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price(self, car: Any) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         return (car.comfort_class
                 * (self.clean_power - car.clean_mark)
                 * self.average_rating / self.distance_from_city_center)
 
-    def wash_single_car(self, car: Any) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
